4e9ca58a9f5351f243677de4e3102c0aa67befe0
Sudeep3339 <72963636+Sudeep3339@users.noreply.github.com>
2020-11-20 15:15:55 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,16 @@
+# +-*/
+# ar are used to to do math operation
+# 2 + 3 = 5
+# result = num1 + num2
+# num1 = 5
+# num2 = 5
+#
+# print(num1 + num2) # 10
+# print(num1 - num2) # 0
+# print(num1 * num2) # 25
+# print(num1 / num2) # 1
+
+print(84937537 + 598453759)
+# 5 5  result   = 5+5
+# result = 5+5 ==> 10
+# result = 10
